d05111e55c3171f0dba9869f9cd1d7503c9bccfd
shreeya123456 <nshreya218@gmail.com>
2025-07-21 12:02:55 +0545

    added images,darkmode/lightmode,and modal in home page

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ document.body.classList.toggle("dark");
 
 
 })
+
+const modal = document.getElementById("myModal");
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.querySelector(".close");
+
+openBtn.onclick = function () {
+  modal.style.display = "block";
+};
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
